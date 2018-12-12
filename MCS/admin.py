904428b392a7c5d8688e756da60ff6df401ac800
ntut105650031 <appymhs5049@gmail.com>
2018-12-12 21:34:36 +0800

    使用者更改完

--- MCS/admin.py
+++ MCS/admin.py
@@ -1,14 +1,28 @@
+# -*- coding: UTF-8 -*-
 from django.contrib import admin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import ugettext_lazy as _
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser, Member, Employee
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
+from .models import CustomUser
 
-admin.site.register(CustomUser, CustomUserAdmin)
-@admin.register(Member)
-@admin.register(Employee)
+
+@admin.register(CustomUser)
+class UserAdmin(DjangoUserAdmin):
+	# 編輯使用者時顯示的欄位
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('last_name', 'first_name','id_TW','phone_number','address','age','gender','discount')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            # 新增 使用者會顯示的欄位
+            'fields': ('email', 'password1', 'password2','last_name', 'first_name','id_TW','phone_number','address','age','gender','discount','groups'),
+        }),
+    )
+    list_display = ('email', 'last_name', 'first_name')
+    search_fields = ('email', 'last_name''first_name')
+    ordering = ('email','id_TW',)
--- MCS/models.py
+++ MCS/models.py
@@ -4,32 +4,88 @@ import uuid
 from django.core.validators import RegexValidator
 from django.core.validators import MaxValueValidator, MinValueValidator
 #
-from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import ugettext_lazy as _
 
 
-class CustomUserManager(UserManager):
-    pass
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('id_TW', 'Y100000000')
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 class CustomUser(AbstractUser):
-    pass
+    GENDER_FEMALE = 'F'
+    GENDER_MALE = 'M'
     
+    id_TW = models.CharField(
+        verbose_name='在台灣的身分證號碼', max_length=10, null=False, unique=True,
+        validators=[RegexValidator(r'^[A-Z]{1}[1-2]{1}[0-9]{8}$')])
+    phone_number = models.CharField(
+        verbose_name='使用者電話號碼', max_length=15, null=False, default='',
+        validators=[RegexValidator(r'^[0-9+()-]+$')])
+    address = models.CharField(
+        verbose_name='使用者住址', max_length=80, null=False,default='')
+    age = models.PositiveIntegerField(
+        verbose_name='使用者年齡', blank=True,default=0)
+
+    GENDER_IN_USER_CHOICES = ((GENDER_FEMALE, 'Female'), (GENDER_MALE, 'Male'),)
+
+    gender = models.CharField(
+        verbose_name='使用者性別', max_length=1, choices=GENDER_IN_USER_CHOICES, null=False, default=GENDER_MALE)
+
+    discount = models.DecimalField(
+        verbose_name='使用者折扣', max_digits=3, decimal_places=2, null=False, default=1.00,
+        validators=[MinValueValidator(0, '0<=使用者折扣<=1'), MaxValueValidator(1, '0<=使用者折扣<=1')])
+    #====================================================
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    last_name = models.CharField(verbose_name='姓', max_length=4, null=False, default='')
+    first_name = models.CharField(verbose_name='名', max_length=8, null=False, default='')
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
 class Member(models.Model):
     # 指向 User 的 primary_key = User.user_id
     user_id = models.OneToOneField(
         CustomUser, verbose_name='會員的使用者編號', on_delete=models.CASCADE, null=False)
 
-    # 會員註冊時自訂的登入帳號
-    member_id = models.CharField(
-        verbose_name='會員編號', max_length=32, null=False, primary_key=True)
+    member_id = models.UUIDField(
+        verbose_name='會員編號', null=False, primary_key=True,
+        default=uuid.uuid4, editable=False)
 
 class Employee(models.Model):
     # 指向 User 的 primary_key = User.user_id
     user_id = models.OneToOneField(
-        CustomUser, verbose_name='員工的使用者編號', on_delete=models.CASCADE, null=False)
+        CustomUser, verbose_name='會員的使用者編號', on_delete=models.CASCADE, null=False)
 
     # admin分配的帳號, 用來登入
-    employee_id = models.CharField(
-        verbose_name='員工編號', max_length=32, null=False, primary_key=True)
+    employee_id = models.UUIDField(
+        verbose_name='員工編號', null=False, primary_key=True,
+        default=uuid.uuid4, editable=False)
 
     title = models.CharField(
         verbose_name='員工職稱', max_length=8, null=False)
