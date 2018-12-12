8b19a943699693418002052b8475552e34302ce2
ntut105650031 <appymhs5049@gmail.com>
2018-12-12 13:24:19 +0800

    更改auth為自定義UserModel 成功(尚未擴充資料)

--- MCS/admin.py
+++ MCS/admin.py
@@ -3,11 +3,12 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
-
+from .models import CustomUser, Member, Employee
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
 
 admin.site.register(CustomUser, CustomUserAdmin)
+@admin.register(Member)
+@admin.register(Employee)
